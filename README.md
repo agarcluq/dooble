6813dedb695e043ab52d35431f7094361bd47b07
agarcluq <agarcluq@everis.com>
2021-02-04 10:42:16 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# dooble-sx3wky
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/dooble-sx3wky)
---
+++ script.js
@@ -0,0 +1,4 @@
+let card1 = document.querySelector(".card1:nth-child(0)").textContent;
+let card2 = document.querySelector(".card2");
+
+console.log(card1);
